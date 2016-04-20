9992493dc4604b243f76e1d9422da4f2c000d927
schaui6 <sanderfer.chau@gmail.com>
2016-04-20 11:04:44 -0700

    Add Input fields, connect to mongo, add contact feature, add remove button

---
+++ public/controllers/controller.js
@@ -0,0 +1,27 @@
+ function AppCtrl($scope, $http){
+    console.log('Hello world from controller')
+
+    var refresh = function() {
+      $http.get('/contactlist').success(function(response){
+          console.log('I got the data I requested');
+          $scope.contactlist = response;
+          $scope.contact = '';  
+      });
+    };
+
+    refresh();
+
+    $scope.addContact = function(){
+      console.log($scope.contact);
+      $http.post('/contactlist', $scope.contact).success(function(response){
+        console.log(response); 
+        refresh();
+    });
+    }
+
+    $scope.remove = function(id){
+      console.log(id);
+      $http.delete('/contactlist/' + id)
+    };
+
+ }
