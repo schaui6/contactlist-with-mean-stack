46bfce85df3fd9b61a5c33056d27a6603d3124c4
schaui6 <sanderfer.chau@gmail.com>
2016-04-21 16:59:28 -0700

    Add edit, update, and clear functionality

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -26,4 +26,22 @@
       });
     };
 
+    $scope.edit = function(id){
+      console.log(id);
+      $http.get('/contactlist/' + id).success(function(response){
+        $scope.contact = response;
+      });
+    };
+
+    $scope.update = function(){
+      console.log($scope.contact._id);
+      $http.put('/contactlist/' + $scope.contact._id, $scope.contact).success(function(response){
+        refresh();
+      }); 
+    };
+
+    $scope.deselect = function(){
+      $scope.contact = "";  
+    }
+
  }
